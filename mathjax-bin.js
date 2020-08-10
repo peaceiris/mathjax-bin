20ccfb01374482558882c64defb541292044b834
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-08-10 21:28:58 +0900

    add base server

--- mathjax-bin.js
+++ mathjax-bin.js
@@ -1,26 +1,47 @@
 const mjAPI = require("mathjax-node");
-const getStdin = require("get-stdin");
+const server = require("server");
+const { post, error } = server.router;
+const { status } = server.reply;
 
-mjAPI.config({
-  MathJax: {
-    // traditional MathJax configuration
-  },
-});
-mjAPI.start();
-
-(async () => {
-  const input = await getStdin();
-
-  mjAPI.typeset(
-    {
-      math: input.trim(),
-      format: "TeX",
-      svg: true,
+server(
+  {
+    port: 8888,
+    security: { csrf: false },
+    parser: {
+      json: { limit: "1mb" },
     },
-    function (data) {
-      if (!data.errors) {
-        console.log(data.svg);
-      }
-    }
-  );
-})();
+  },
+  [
+    post("/", (ctx) => {
+      console.log(ctx.data);
+      const buff = new Buffer.from(ctx.data.input, "base64");
+      const decede = buff.toString("ascii");
+      return decede;
+    }),
+  ],
+  error((ctx) => status(500).send(ctx.error.message))
+);
+
+// mjAPI.config({
+//   MathJax: {
+//     // traditional MathJax configuration
+//   },
+// });
+// mjAPI.start();
+
+// (async () => {
+//   const input = await getStdin();
+
+//   mjAPI.typeset(
+//     {
+//       math: input.trim(),
+//       format: "TeX",
+//       svg: true,
+//     },
+//     function (data) {
+//       if (!data.errors) {
+//         console.log(data.svg);
+//       }
+//     }
+//   );
+// })();
