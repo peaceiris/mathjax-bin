d7f71d3fd9d687784cfb9597b334980c6dd7a7ae
Shohei Ueda <30958501+peaceiris@users.noreply.github.com>
2020-08-11 23:17:47 +0900

    change basic spec to cli (#3)

---
+++ mathjax-bin.js
@@ -0,0 +1,27 @@
+const mjAPI = require("mathjax-node");
+const getStdin = require("get-stdin");
+
+mjAPI.config({
+  MathJax: {
+    // traditional MathJax configuration
+  },
+});
+mjAPI.start();
+
+(async () => {
+  const base64Str = await getStdin();
+  const math = new Buffer(base64Str, "base64");
+
+  mjAPI.typeset(
+    {
+      math: math,
+      format: "TeX",
+      svg: true,
+    },
+    function (data) {
+      if (!data.errors) {
+        console.log(data.svg);
+      }
+    }
+  );
+})();
